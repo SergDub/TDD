6059970dad92fbb11dbde02fbc0b664a766a2799
Сергей Дубовский <svdub@mail.ru>
2023-03-07 12:12:50 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,7 @@
 public class PhoneBook {
+
+    public static int add(){
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+    @Test
+    void add() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertNotEquals(phoneBook.add(), 0);
+    }
+
+
 }
